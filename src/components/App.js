378193a0521644a5922a63989838a493202f2bfa
Arda Ozcan <ardaoezcan@gmail.com>
2018-11-06 16:16:54 +0100

    home laptop

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,36 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sampleFishes';
 
 class App extends React.Component {
+  constructor() {
+    super(); //once we write 'super()' then we can use the 'this' below
+    this.addFish = this.addFish.bind(this); //this basically makes addFish method available for App component
+    this.loadSamples = this.loadSamples.bind(this); //again, we do this so that we can use 'this' in the methods below. for example, on the loadSamples method below
+    this.state = {
+      fishes: {},
+      order: {}
+    }
+  }
+
+  addFish(fish) {
+    // update our state
+    //1. first get the current State
+    const fishes = {...this.state.fishes};
+    //2. add in our new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish; 
+    // set state
+    this.setState({ fishes }); //Long version is: this.setState({ fishes: fishes });
+  }
+
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +39,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
   }
