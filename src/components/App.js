9b2bfa92c0484bf39b1e71a1a5b3c7a6a1ffbd0f
Arda Ozcan <ardaoezcan@gmail.com>
2018-11-07 08:17:09 +0100

    fish list populated

--- src/components/App.js
+++ src/components/App.js
@@ -39,7 +39,11 @@ class App extends React.Component {
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
           <ul className="list-of-fishes">
-            <Fish />
+            {
+              Object
+                .keys(this.state.fishes)
+                .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+            }
           </ul>
         </div>
         <Order />
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 class Fish extends React.Component {
   render() {
+    const { details } = this.props;
     return (
-      <li className="menu-fish">Fish is my name</li>
+      <li className="menu-fish">
+        <img src={details.image} alt={details.name} />
+        <h3 className="fish-name">
+          {details.name}
+          <span className="price">{details.price}</span>
+        </h3>
+        <p>{details.desc}</p>
+        <button>Add To Order</button>
+      </li>
     );
   }
 }
